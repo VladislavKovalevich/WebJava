17c71e62ee9baf759b5f0942668a09c7ebf8d84d
Vlad <vlad_kov@tut.by>
2019-09-28 22:43:33 +0300

    firstcommit

---
+++ src/users/Admin.java
@@ -0,0 +1,15 @@
+package users;
+
+public class Admin {
+
+    private String name;
+    private boolean isAdmin;
+
+    void Admin(String name, boolean isAdmin)
+    {
+        this.isAdmin = isAdmin;
+        this.name = name;
+    }
+
+
+}
---
+++ src/users/IAdminFunction.java
@@ -0,0 +1,8 @@
+package users;
+
+import tour.NewTour;
+
+public interface IAdminFunction {
+    void DeleteTour();
+    NewTour CreateNewTour();
+}
